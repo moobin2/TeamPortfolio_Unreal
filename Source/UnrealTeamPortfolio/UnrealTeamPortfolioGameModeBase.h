6f814a5e448fa73722ae8e2354f5f83c74b834b5
moobin2 <anqls20000@naver.com>
2017-08-15 06:41:20 +0900

    초기 커밋
    프로젝트 기본틀 셋팅

---
+++ Source/UnrealTeamPortfolio/UnrealTeamPortfolioGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealTeamPortfolioGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALTEAMPORTFOLIO_API AUnrealTeamPortfolioGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
